706dec8fd64e2d20a9d90bf7fee186d2b2a28732
aicha0k <aicha.k2002@gmail.com>
2022-12-09 19:03:10 -0300

    meu teste

--- main.py
+++ main.py
@@ -1,78 +1,58 @@
 import cv2
 import numpy as np
-import datetime
+import datetime as dt
 import pathlib
 import shutil
 
-current_date = datetime.datetime.today().strftime("%d-%m-%Y")
-avaible_devices = []
-for i in range(0,10):
+current_date = dt.datetime.today().strftime('%d-%m-%Y')
+#checking in which usb cameras are connected
+connected_cameras = []
+for i in range(0,5):
     try:
         cap = cv2.VideoCapture(i)
         if cap.isOpened():
-            avaible_devices.append(i)
+            connected_cameras.append(i)
         cap.release()
     except:
         continue
-print(avaible_devices)
-if(len(avaible_devices) != 0):
-    auxillary_cam = None
-    webcam = None
-    for i in range(0, len(avaible_devices)):
-        cam = cv2.VideoCapture(avaible_devices[i])
-        if(float(cam.get(cv2.CAP_PROP_FPS)) == 14.999992):
-            webcam = cam
-            continue
-        if(float(cam.get(cv2.CAP_PROP_FPS)) == 5.0):
-            auxillary_cam = cam
-            continue
-        if(auxillary_cam != None and webcam != None):   #if both cameras are found,
-            break
-        cam.release()
+#deciding which one is the main camera
+cam1 = None
+cam2 = None
+print(connected_cameras)
+for i in range(len(connected_cameras)):
+    cap = cv2.VideoCapture(connected_cameras[i])
+    my_fps = float(cap.get(cv2.CAP_PROP_FPS))
+    print(cap.get(cv2.CAP_PROP_FPS))
+    if (my_fps == 14.999992):
+        cam1 = connected_cameras[i]
+        continue
+    if (my_fps== 1.0):
+        cam2 = connected_cameras[i]
+        continue
+    cap.release()
 
-    height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
-    fps = float(webcam.get(cv2.CAP_PROP_FPS))   #consertar fps, video muito acelerado
-    print("props camera 1")
-    print(height, width, fps)
+webcam = cv2.VideoCapture(cam1)
+auxiliary_cam = cv2.VideoCapture(cam2)
+height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
+fps = float(webcam.get(cv2.CAP_PROP_FPS))   #consertar fps, video muito acelerado
     # capturing two videos at same time. two cameras
-    height2 = int(auxillary_cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    width2 = int(auxillary_cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-    fps2 = float(auxillary_cam.get(cv2.CAP_PROP_FPS))
-    print("props camera 2")
-    print(height2, width2, fps2)
-
-name = input("digite o nome da crianca: ")
-movement = input("movimento: ")
-path =pathlib.Path(f"dataset/{name}/{movement}")
-if path.exists():
-    shutil.rmtree(path)
-    path.mkdir(parents=True, exist_ok=False)    
-else:
-    path.mkdir(parents=True, exist_ok=False)    # if the folder doesn't exist, create it
-
-writer = cv2.VideoWriter(f"num{movement}.mov", fourcc=cv2.VideoWriter_fourcc(*"DIVX"), fps=fps, frameSize=(width, height))
-writer2 = cv2.VideoWriter(f"num{movement}aux.mov", fourcc=cv2.VideoWriter_fourcc(*"DIVX"), fps=fps2, frameSize=(width2, height2))
-with open(f"dataset/{name}/{movement}/info.txt", "w") as file:
-    file.write(f"nome: {name},movimento numero: {movement}, data: {current_date}")
+height2 = int(auxiliary_cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width2 = int(auxiliary_cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+fps2 = float(auxiliary_cam.get(cv2.CAP_PROP_FPS))
+print(f'fps da camera principal: {fps} fps da camera auxiliar: {fps2}')
 
 while True:
     ret, frame = webcam.read()
-    ret2, frame2 = auxillary_cam.read()
-    cv2.imshow("main", frame)
-    cv2.imshow("auxillary", frame2)
-    writer.write(frame)
-    writer2.write(frame2)
+    ret2, frame2 = auxiliary_cam.read()
+    if ret:
+        cv2.imshow("main", frame)
+    if ret2:
+        cv2.imshow("auxillary", frame2)
     if cv2.waitKey(1) & 0xFF == ord(" "):
         break
 
-shutil.move(f"num{movement}.mov", f"dataset/{name}/{movement}")
-shutil.move(f"num{movement}aux.mov", f"dataset/{name}/{movement}")
-shutil.move(f"dataset/{name}/{movement}/info.txt", f"dataset/{name}/{movement}")
-
 webcam.release()
-auxillary_cam.release()
-writer.release()
-writer2.release()
+auxiliary_cam.release()
 
 cv2.destroyAllWindows()
